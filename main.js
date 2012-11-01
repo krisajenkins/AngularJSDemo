ac98639ebecfb3a2784b2a0fefba48875f6540bb
Kris Jenkins <krisajenkins@gmail.com>
2012-11-01 15:15:36 +0000

    Adding a product controller.
    ...to let us access sub-images more easily.

--- main.js
+++ main.js
@@ -5,4 +5,10 @@ angular.module('ProductsApp', ['RegexpFilterModule'])
 	.controller('ProductListController', function ($scope) {
 		$scope.query = "";
 		$scope.products = productQuery.products;
+	})
+	.controller('ProductController', function ($scope) {
+		$scope.product.images.map(function (item) {
+			var imageSizeName = item.sizeName.toLowerCase();
+			$scope.product.images[imageSizeName] = item;
+		});
 	});
