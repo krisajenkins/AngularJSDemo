02d234626650562adfa878d2e13a0f0be1958f7b
Kris Jenkins <krisajenkins@gmail.com>
2012-11-01 18:00:03 +0000

    Adding a sub-page to demonstrate $routeProvider.

--- main.js
+++ main.js
@@ -2,17 +2,30 @@
 "use strict";
 
 angular.module('ProductsApp', ['RegexpFilterModule', 'ShopApiModule', 'ngSanitize'])
+	.factory('productTranslator', function () {
+		return function (product) {
+			product.image = product.images.map(function (item) {
+				var imageSizeName = item.sizeName.toLowerCase();
+				product.images[imageSizeName] = item;
+			});
+
+			return product;
+		};
+	})
 	.controller('ProductListController', function ($scope, ShopApi) {
 		$scope.query = "";
 		$scope.data = ShopApi.search({endpoint: 'apiSearch', cat: 'mens-ties'});
 	})
-	.controller('ProductController', function ($scope) {
-		if ($scope.product.images) {
-			$scope.product.images.map(function (item) {
-				var imageSizeName = item.sizeName.toLowerCase();
-				$scope.product.images[imageSizeName] = item;
-			});
-		}
+	.controller('ProductController', function ($scope, productTranslator) {
+		$scope.product = productTranslator($scope.product);
+	})
+	.controller('ProductDetailController', function ($scope, ShopApi, $routeParams, productTranslator) {
+		ShopApi.search(
+			{endpoint: 'apiSearch', cat: 'mens-ties', prodId: $routeParams.productId},
+			function (data) {
+				$scope.product = productTranslator(data.products[0]);
+			}
+		);
 	})
 	.directive('productSummary', function () {
 		return {
@@ -39,5 +52,6 @@ angular.module('ProductsApp', ['RegexpFilterModule', 'ShopApiModule', 'ngSanitiz
 	.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider
 			.when('/', {templateUrl: 'templates/front_page.html', controller: 'ProductListController'})
+			.when('/product/:productId', {templateUrl: 'templates/product_detail.html', controller: 'ProductDetailController'})
 			.otherwise({redirectTo: '/'});
 	}]);
